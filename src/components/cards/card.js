7dd5b3f2675291adffd53b105ef94df97f95df7f
Ayush Srivastava <ayushs.1992@gmail.com>
2020-02-05 23:05:11 +0530

    Added cards

--- src/components/cards/card.js
+++ src/components/cards/card.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Card() {
-  return <div />;
+  return (
+    <div className="card">
+      <div className="card-header">Header</div>
+      <div className="card-body">Body</div>
+    </div>
+  );
 }
 
 export default Card;
